02426ae13862a2533833cab241f88563a6fc05ad
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-17 14:43:41 +0100

    home controller (page_home)

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Music;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+
+        $categories = Category::all();
+        $musicx = Music::all();
+
+
+        return view('index', compact('categories','musicx'));
+    }
+}
