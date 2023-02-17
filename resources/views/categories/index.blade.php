8df8673464597ae5ec955520d37c629b7b7e19db
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-17 14:51:01 +0100

    page display categorie

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Categorieshhhh') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="container">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <a class="btn btn-success" href="{{ route('categories.create') }}">Add new category</a>
+                        <br /><br />
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th>Image</th>
+                                    <th class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($categories as $category)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+                                    <td>{{ $category->name }}</td>
+                                    <td>{{ $category->description }}</td>
+                                    <td><img src="/images/categories/{{ $category->image }}"></td>
+                                    <td class="d-flex text-center" style="justify-content: center;">
+                                        <a class="btn btn-primary" href="{{ route('categories.edit', $category) }}">Update</a>
+                                        <form method="POST" action="{{ route('categories.destroy', $category) }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger ms-3" onclick="return confirm('Are you sure?')">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
