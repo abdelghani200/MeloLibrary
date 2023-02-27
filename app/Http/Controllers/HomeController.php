ebbaf004620036072166affe39a4fdfeb9460b48
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-27 17:28:11 +0100

    add categories in page home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,14 @@ class HomeController extends Controller
     {
 
         $categories = Category::all();
-        $musicx = Music::all();
+        $musicx = Music::when(request('category_id'),function($query) {
+            $query->where('category_id', request('category_id'));
+        })->latest()->get();
 
 
         return view('index', compact('categories','musicx'));
     }
+    
+
 }
+
