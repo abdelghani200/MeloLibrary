dd7b5faf016256cf21aee963ac01a2133859a876
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-03-24 13:42:32 +0000

    add comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Music;
 use App\Models\Category;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,12 +13,14 @@ class HomeController extends Controller
     {
 
         $categories = Category::all();
+        $comment = Comment::all();
+
         $musicx = Music::when(request('category_id'),function($query) {
             $query->where('category_id', request('category_id'));
         })->latest()->get();
 
 
-        return view('index', compact('categories','musicx'));
+        return view('index', compact('categories','musicx','comment'));
     }
     
 
